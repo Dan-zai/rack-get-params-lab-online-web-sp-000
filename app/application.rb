1f3dfe0471c4d881ab11d18ecd852ebd39e4efe2
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-04-02 03:28:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -22,6 +22,8 @@ class Application
     end
   end
   elsif req.path.match(/add/)
+    search_term = req.params["item"]
+    resp.write handle_search(search_term)
     else
       resp.write "Path Not Found"
     end
